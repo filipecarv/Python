cd5166fd48a269a5ba3d187213a89366962dc8b0
Filipe <104007477+filipecarv@users.noreply.github.com>
2024-03-15 14:30:43 -0300

    Add files via upload

---
+++ Qst06.py
@@ -0,0 +1,5 @@
+Dia = input('Diga o dia que voce nasceu:')
+Mes = input('Diga o mês que voce nasceu:')
+Ano = input('Diga o ano que voce nasceu:')
+Nascimento = Dia + "/" + Mes + "/" + Ano
+print('Voce nasceu em:',Nascimento)
