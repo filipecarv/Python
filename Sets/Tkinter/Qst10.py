2d412ace8bf9f3cf0575dca670ac4682b50cb73a
Filipe <104007477+filipecarv@users.noreply.github.com>
2024-04-22 01:38:03 -0300

    Delete Sets/Tkinter directory

--- Sets/Tkinter/Qst10.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-janela = Tk()
-janela.title('Imagem!')
-janela['bg'] = 'lightgray'
-janela.geometry('800x450')
-img = PhotoImage(file='Imagem.png')
-image = Label(janela, image=img)
-image.pack()
-janela.mainloop()
