7d895dd1455da59660d617c62af5fd13b58f6373
Filipe <104007477+filipecarv@users.noreply.github.com>
2024-04-22 01:32:01 -0300

    Delete Atividade de Desktop directory

--- Atividade de Desktop/Qst01.py
+++
@@ -1,7 +0,0 @@
-from tkinter import*
-janela = Tk()
-janela.title('Bom dia ai po ce doido!')
-janela['bg'] = 'lightgray'
-janela.geometry("300x200+550+100")
-janela.resizable(width=False, height=False)
-janela.mainloop()
--- Atividade de Desktop/Qst07.py
+++
@@ -1,8 +0,0 @@
-from tkinter import*
-janela = Tk()
-janela.title('Ta ai o espaçamento interno meu bom!')
-janela.geometry('400x400+300+50')
-janela['bg']='lightgray'
-Bt1 = Button(janela, text='Espaçamento Interno', font = 'bold', bg='lightgrey',width= 30, height= 40)
-Bt1.pack(padx=(10,10), pady=(120,120))
-janela.mainloop()
